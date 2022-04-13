58e2eec5def0d15c922189ed5bb68f9d5a0155e9
aluno <aluno@DSN1251017059>
2022-04-13 15:10:27 -0300

    terceiro commit

--- OrientacaoObjeto/src/Pessoa.java
+++ OrientacaoObjeto/src/Pessoa.java
@@ -8,8 +8,24 @@ public class Pessoa {
 		this.numFigurinhas += numFigurinhas;	
 	}
 	
-	void dar() {
-	
+	void dar(int numFigurinhas, Pessoa pessoa) {
+		
+		if (this.numFigurinhas < numFigurinhas) {
+			System.out.println(" ");
+			System.out.println("Você Não tem a quantidade de figurinhas necessárias e nao podera dar figuinhas para " + pessoa.nome);
+			System.out.println("");
+			System.out.println("Então ");
+		  }
+			else {		
+
+				this.numFigurinhas -= numFigurinhas;
+				
+				pessoa.receber(numFigurinhas);
+				System.out.println("");
+				System.out.println("Você tem a quantidade de figurinhas necessárias e poderá dar figurinhas para " + pessoa.nome);
+				System.out.println("");
+				System.out.println("Então");
+		}
 	}
 	
 	
--- OrientacaoObjeto/src/main.java
+++ OrientacaoObjeto/src/main.java
@@ -10,8 +10,29 @@ public class main {
 	
 		pessoa1.receber(3);
 		
-		System.out.println(pessoa1.nome);
-		System.out.println(pessoa1.numFigurinhas);
+		System.out.println(pessoa1.nome + " tem " + pessoa1.numFigurinhas + " figurinhas ");
+		
+		System.out.println("E");
+		
+		
+		Pessoa pessoa2 = new Pessoa();
+		
+		pessoa2.nome = "mdn";
+		pessoa2.numFigurinhas = 2;
+		
+		System.out.println(pessoa2.nome + " tem " + pessoa2.numFigurinhas + " figurinhas ");
+	
+		
+		pessoa1.dar(5, pessoa2);
+	
+		System.out.println(pessoa1.nome + " ficou com " + pessoa1.numFigurinhas + " figurinhas ");
+	
+		System.out.println("E");
+		
+		System.out.println(pessoa2.nome + " ficou com " + pessoa2.numFigurinhas + " figurinhas ");
+		
+		
+		
 		
 		
 	}
