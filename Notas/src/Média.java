fc170f001a705f3160ad831a654bdf80b442fa4e
aluno <aluno@DSN1251017059>
2022-04-13 15:22:45 -0300

    hj

---
+++ Notas/src/Média.java
@@ -0,0 +1,29 @@
+
+public class Média {
+
+	public static void main(String[] args) {
+		
+		double nota1;
+		double nota2;
+		double nota3;
+		double média;
+		
+		nota1 = 8.5;
+		nota2 = 7.5;
+		nota3 = 6.0;
+		
+		System.out.println(" média ");
+		System.out.println("Nota1: " + nota1);
+		System.out.println("Nota2: " + nota2);
+		System.out.println("Nota3: " + nota3);
+		System.out.println("");
+		System.out.println("Sua media é: ");
+		
+		média = nota1 + nota2 + nota3;
+		
+		System.out.println(média /3);
+	
+
+	}
+
+}
