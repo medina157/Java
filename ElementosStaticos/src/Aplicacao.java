eb2fd879291ddd3fd644eec74f4ed2ccecd27f78
aluno <aluno@DSN1251230719>
2022-05-02 11:36:11 -0300

    666

---
+++ ElementosStaticos/src/Aplicacao.java
@@ -0,0 +1,40 @@
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Matematica matematica = new Matematica();
+		
+//		int resultado = matematica.somar(2,3);
+//		System.out.println((resultado));
+//		resultado = matematica.subtrair(10, 4);
+//		System.out.println(resultado);
+		
+		
+//		int resultado = Matematica.somar(2, 4) ;
+//		System.out.println(resultado);
+//		
+//		resultado = Matematica.subtrair(2, 4) ;
+//		System.out.println(resultado);
+		
+//		Contador contador1 = new Contador();
+//		Contador contador2 = new Contador();
+//		Contador contador3 = new Contador();
+//		contador1.incrementar();
+//		System.out.println(contador1.getValor());
+//		System.out.println(contador2.getValor());
+//		System.out.println(contador3.getValor());
+//	
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		System.out.println(Contador.getValor());
+		
+		System.out.println(Constantes.MEDIA_DA_PROVA);
+	}
+
+
+
+		
+		}
+	
+
