1d0388967ced759f775ea8b1e9cd0ebc91295678
medina157 <102966250+medina157@users.noreply.github.com>
2022-05-18 20:11:36 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,7 +1,7 @@
 
-public class Cachorro extends Animal {
+public class Cachorro extends Animal { // extends significa que a classe cachorro esta estendendo de animal.
 
-	@Override
+	@Override //  Reescre um metodo que foi herdado, onde a seu comportamento na classe pai é diferente do seu comportamento na classe filha,faz comq eles possuem o msm nome mas suas ações sejam distintas.
 	public void Falar() {
 		System.out.println("Au-Au");
 	}
