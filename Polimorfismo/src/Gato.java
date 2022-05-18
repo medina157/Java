90885f03c33fed2d112b7f888765720c219d8058
medina157 <102966250+medina157@users.noreply.github.com>
2022-05-18 20:12:28 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -3,7 +3,7 @@ public class Gato extends Animal {
 
 	@Override
 	public void Falar() {
-		System.out.println("MIAU");
+		System.out.println("MIAU"); // a ação do gato que aparecera para o usuario 
 	}
 
 	
