477c69361eac5f5f01995fe1aa071e096ae6faee
medina157 <102966250+medina157@users.noreply.github.com>
2022-06-06 08:47:31 -0300

    Update Aplicacao.java

--- ElementosStaticos/src/Aplicacao.java
+++ ElementosStaticos/src/Aplicacao.java
@@ -4,9 +4,9 @@ public class Aplicacao {
 		
 		Matematica matematica = new Matematica();
 		
-//		int resultado = matematica.somar(2,3);
-//		System.out.println((resultado));
-//		resultado = matematica.subtrair(10, 4);
+//		int resultado = matematica.somar(2,3);   // //criando uma variavel, recebe um valor de um objeto junto com metodo somar
+//		System.out.println((resultado)); //mostra o resultado
+//		resultado = matematica.subtrair(10, 4);//a variavel resultado recebe o valor do objeto com o metodo subtrair
 //		System.out.println(resultado);
 		
 		
@@ -16,7 +16,7 @@ public class Aplicacao {
 //		resultado = Matematica.subtrair(2, 4) ;
 //		System.out.println(resultado);
 		
-//		Contador contador1 = new Contador();
+//		Contador contador1 = new Contador(); //criando um objeto
 //		Contador contador2 = new Contador();
 //		Contador contador3 = new Contador();
 //		contador1.incrementar();
