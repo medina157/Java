b3ae54a3dccd8c902595b76540ae6ab7f8bdc622
medina157 <102966250+medina157@users.noreply.github.com>
2022-06-06 08:48:23 -0300

    Update Constantes.java

--- ElementosStaticos/src/Constantes.java
+++ ElementosStaticos/src/Constantes.java
@@ -4,6 +4,6 @@ public class Constantes {
 //	public static final double MEDIA_DA_PROVA = 7,0;
 	public static final double MEDIA_DA_PROVA;
 	static{
-		MEDIA_DA_PROVA = 7.0;
+		MEDIA_DA_PROVA = 7.0; //recebe um valor fixo
 	}
 }
