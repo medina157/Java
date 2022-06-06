87b53fd3111518b29dbde0ccef3492a03a801b9c
medina157 <102966250+medina157@users.noreply.github.com>
2022-06-06 08:08:03 -0300

    Update main.java

--- Cadastro/src/main.java
+++ Cadastro/src/main.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 public class main {
 	public static void main(String[] args) throws IOException {
 
-		try (Scanner entrada = new Scanner(System.in)) {
-			List<Pessoa> lista = new ArrayList<Pessoa>();
+		try (Scanner entrada = new Scanner(System.in)) { // Scanner para separar a entrada dos textos,comm isso gera tokens
+			List<Pessoa> lista = new ArrayList<Pessoa>();    //List e uma interface e arrayList e a classe que a implementa
 
-			for (int x = 0; x < 4; x++) {
+			for (int x = 0; x < 4; x++) {    // e um laço de repetição que tem que determinar a quantidade de vezes que ele ira se repetir
 
 				Pessoa pessoa = new Pessoa();
 				pessoa.setEndereco(new Endereco());
@@ -27,7 +27,7 @@ public class main {
 
 				System.out.print("Digite seu sexo 1-Masculino 2-Feminino: 1");
 				int opcao = 0;
-				while (opcao != 1 && opcao != 2) {
+				while (opcao != 1 && opcao != 2) {     //while e o laço repetição que enquanto o valor seja digitado errado ele vai voltar ate digitar o valor correto
 					opcao = entrada.nextInt();
 					if (opcao == 1)
 						pessoa.setSexo(Sexo.MASCULINO);
@@ -35,14 +35,14 @@ public class main {
 					else if (opcao == 2)
 						pessoa.setSexo(Sexo.FEMININO);
 					else {
-						System.out.println("Esse sexo n�o existe");
+						System.out.println("Esse sexo não existe");
 						System.out.println("Digite seu sexo 1-Masculino 2-Feminino");
 					}
 				}
-				System.out.print("Digite onde bairro onde voc� mora: ");
+				System.out.print("Digite onde bairro onde você mora: ");
 				pessoa.getEndereco().setBairro(entrada.next());
 
-				System.out.print("Digite o nome da rua onde voc� mora: ");
+				System.out.print("Digite o nome da rua onde você mora: ");
 				pessoa.getEndereco().setRua(entrada.next());
 
 				System.out.print("Digite o numero de sua casa: ");
@@ -61,7 +61,7 @@ public class main {
 
 				}
 				try (BufferedReader reader = new BufferedReader(new FileReader("saida.txt"))) {
-					String line;
+					String line;     //ele inclui comandos para liberar recursos que podem ter sido alocados
 
 					while ((line = reader.readLine()) != null) {
 						Pessoa pessoa = new Pessoa(line);
