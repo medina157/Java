900ba6e256fb2f054a9e3bdd085c2f0d2ea877d3
medina157 <102966250+medina157@users.noreply.github.com>
2022-06-06 08:42:09 -0300

    Update ContaInvestimento.java

--- ContaBancaria/src/ContaInvestimento.java
+++ ContaBancaria/src/ContaInvestimento.java
@@ -1,15 +1,14 @@
 
 public class ContaInvestimento extends ContaBancaria{
 	
-	//A conta investimento começa com 0, igual na vida real que você precisa transferir dinheiro da sua conta corrente para a conta de investimentos.
-	private double saldo = 0;
+	private double saldo = 0;//A conta investimento comeÃ§a com 0, vocÃª precisa transferir o dinheiro da conta corrente para a conta de investimentos.
+
 	
 
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
-	//Métodos com Override
 	@Override
 	public void depositar(double valor) {
 		// TODO Auto-generated method stub
